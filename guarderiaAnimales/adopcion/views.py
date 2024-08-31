2a8a14766b8b70431d6595562046e284b52ef68d
Daniel <danielqqq555@gmail.com>
2024-08-31 17:48:04 -0500

    luego de importar los datos en el view

--- guarderiaAnimales/adopcion/views.py
+++ guarderiaAnimales/adopcion/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Mascota
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hola, mundo!')
+    mascotas = Mascota.objects.all
+    context = {'mascotas':mascotas}
+
+    return render(request,'adopcion/index.html', context)
