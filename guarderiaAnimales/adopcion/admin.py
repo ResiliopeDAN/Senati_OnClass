c9e530092cf3e692e7ba8e481beadd63524fe5e0
Daniel <danielqqq555@gmail.com>
2024-08-31 17:05:05 -0500

    luego de migrar los modelos en el dashboard

--- guarderiaAnimales/adopcion/admin.py
+++ guarderiaAnimales/adopcion/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from adopcion.models import Mascota
+from adopcion.models import Interesados
 # Register your models here.
+
+admin.site.register(Mascota)
+admin.site.register(Interesados)
